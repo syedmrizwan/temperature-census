3c8d43780a6fcd373a48351febaf682d38de8bc7
syedmrizwan <syed.m.rizwan@outlook.com>
2020-06-10 00:15:10 +0500

    Restructure project

---
+++ src/go.mod
@@ -0,0 +1,10 @@
+module temperature-census
+
+go 1.14
+
+require (
+	contrib.go.opencensus.io/exporter/prometheus v0.2.0
+	github.com/go-resty/resty/v2 v2.3.0
+	github.com/tidwall/gjson v1.6.0
+	go.opencensus.io v0.22.3
+)
---
+++ src/model/model.go
@@ -0,0 +1,14 @@
+package model
+
+type TemperatureDetail struct {
+	Temperature float64 `json:"temperature"`
+	FeelsLike   float64 `json:"feels_like"`
+	Pressure    float64 `json:"pressure"`
+	Humidity    float64 `json:"humidity"`
+}
+
+type TemperatureData struct {
+	TemperatureDetail TemperatureDetail
+	Err               error
+}
+
